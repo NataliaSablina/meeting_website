0474c42bc6ab8e219f8d4d889ab7c1c4032986fb
NataliaSablina <nataliasablina145@gmail.com>
2022-09-28 13:26:28 +0200

    add jwt authorization

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
 from rest_framework import routers
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from user.views import UserViewSet
 
 router = routers.SimpleRouter()
@@ -7,6 +9,8 @@ router.register(r"user", UserViewSet)
 
 
 urlpatterns = [
-    path("authentication/", include("rest_framework.urls"), name="authentication")
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 urlpatterns += router.urls
